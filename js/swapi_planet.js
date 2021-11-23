15f2f4c4911db5f468f03fb63d62a0cd46e31c8d
harold Pierrache <h.pierrache@gmail.com>
2021-11-23 11:56:27 +0100

    starwarsApi3

--- js/swapi_planet.js
+++ js/swapi_planet.js
@@ -5,7 +5,8 @@ let planetHttpRequest = new XMLHttpRequest();
                 let response = JSON.parse(planetHttpRequest.responseText);
                 let ul = document.getElementById("planet")
                     for(planet of response.results) {
-                        ul.innerHTML += `<li class="list-group-item list-group-item-warning">Planet: ${planet.name}</li>`
+                        ul.innerHTML += `<li class="list-group-item list-group-item-warning"><h2 class="text-dark p-2">Planet:</h2> <h1 class="text-danger"><strong>${planet.name}</strong></h1></li>`
+                        ul.innerHTML += `<li class="list-group-item list-group-item-warning"><p style="color:blue"><h3><p class="text-danger p-2">Diameter: <p><strong>${planet.diameter}</strong></p> </p><p class="text-danger">Climate:</p><p> <strong>${planet.climate}</strong></p></h3></li>`
                     }
             }
         }
